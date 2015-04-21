10ad2de69c1242443db19965c3c4209284a19959
Wes Williams <wwilliams@pivotal.io>
2015-04-20 21:57:06 -0400

    Refactor events from functions to server jars

--- workshop/projects/GemServer/src/main/java/io/pivotal/eventhandlers/SimpleWriter.java
+++ workshop/projects/GemServer/src/main/java/io/pivotal/eventhandlers/SimpleWriter.java
@@ -2,7 +2,9 @@ package io.pivotal.eventhandlers;
 
 import io.pivotal.domain.Person;
 
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 
 import com.gemstone.gemfire.cache.Declarable;
 import com.gemstone.gemfire.cache.EntryEvent;
@@ -11,30 +13,68 @@ import com.gemstone.gemfire.internal.cache.EntryEventImpl;
 import com.gemstone.gemfire.pdx.PdxInstance;
 import com.gemstone.gemfire.pdx.WritablePdxInstance;
 
-public class SimpleWriter extends CacheWriterAdapter<String, Person> implements Declarable {
+public class SimpleWriter<K,V> extends CacheWriterAdapter<K,V> implements Declarable {
 
-	 public void beforeCreate(EntryEvent<String, Person> event) {
-
+	private String fromName = null;
+	private String toName = null;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.gemstone.gemfire.cache.util.CacheWriterAdapter#beforeCreate(com.gemstone.gemfire.cache.EntryEvent)
+	 */
+	@Override
+	 public void beforeCreate(EntryEvent<K, V> event) {
+		 /*
+		  * update the name in fromName to toName when inserting a new record
+		  */
 		 EntryEventImpl eventImpl = (EntryEventImpl) event;
-
 		 Object cachedPerson = event.getNewValue();
+		 
 		 if (cachedPerson instanceof PdxInstance) {
-
-			 PdxInstance pdxPerson = (PdxInstance) cachedPerson;
-
-			 // Update a field and put it back into the cache
-			 // without deserializing the entire object
-			 WritablePdxInstance writablePdxPerson = pdxPerson.createWriter();
-			 writablePdxPerson.setField("name", "Fulano");
-			 eventImpl.setNewValue((PdxInstance) writablePdxPerson);
+			 PdxInstance pdxPerson = (PdxInstance) cachedPerson;			 
+			 String name = (String) pdxPerson.getField("name");
+			 
+			 if (name != null && name.endsWith(fromName)) {
+				 // Update a field and put it back into the cache
+				 // without deserializing the entire object
+				 // pretty amazing stuff that no other in-memory product does
+				 WritablePdxInstance writablePdxPerson = pdxPerson.createWriter();
+				 writablePdxPerson.setField("name", name.substring(0, name.indexOf(" ")+1) + toName);
+				 eventImpl.setNewValue((PdxInstance) writablePdxPerson);
+			 }
 		 }
-		 else {
-			 Person person =  (Person) event.getNewValue();
-			 person.setName("Fulano");
+		 else if (cachedPerson instanceof Person) {
+			 Person person = (Person) event.getNewValue();
+			 if (person.getName().endsWith(fromName)) {
+				 person.replaceLastName(toName);
+			 }
 		 }
 		 eventImpl.makeSerializedNewValue();
 	 }
+	
+	@Override
+	 public void beforeUpdate(EntryEvent<K, V> event) {
+	}
 
-	public void init(Properties arg0) {
+	public void init(Properties props) {
+		 Set<Object> propKeys = props.keySet();
+		 Iterator<Object> propKeyIter = propKeys.iterator();
+		 while (propKeyIter.hasNext()) {
+			 String key = (String) propKeyIter.next();
+			 if (key.equalsIgnoreCase("fromName")) {
+				 setfromName(props.getProperty(key));
+			 }
+			 else if (key.equalsIgnoreCase("toName")) {
+				 settoName(props.getProperty(key));
+			 }
+		 }
+	}
+	
+	private void settoName(String toName) {
+		this.toName = toName;
+	}
+	
+	private void setfromName(String fromName) {
+		this.fromName = fromName;
 	}
 }
